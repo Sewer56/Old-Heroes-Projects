ec6509d259710b88c1b07bd801262d20ce213bb6
Sewer <sewer56lol@googlemail.com>
2016-08-31 18:56:56 +0100

    Windows/CMD: Implement Audio Conversion Engine #2

--- AudioTest.bat
+++ AudioTest.bat
@@ -2,3 +2,7 @@ set WORKING_DIRECTORY=%cd%
 for %%f in (ROM\*.adx) do (copy /Y %%f Tools\AudioConversion\0ADX2WAV\)
 cd Tools\AudioConversion\0ADX2WAV\
 call ADX2WAV.bat
+mkdir %WORKING_DIRECTORY%\ROM\BGM
+for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2\*.m4a) do (move /Y %%f %WORKING_DIRECTORY%\ROM\BGM)
+for %%f in (%WORKING_DIRECTORY%\ROM\*.adx) do (del /f %%f)
+pause
