35c561388185657b5dd284b95e2cfc6c96402207
Sewer <sewer56lol@googlemail.com>
2016-09-02 22:39:33 +0100

    Remove VGMStream from the project, return ADX2M4A script for BGM.

--- SetupFromROM.bat
+++ SetupFromROM.bat
@@ -3,11 +3,18 @@ rem MASTER CODE XDDDDDD
 rem 
 set WORKING_DIRECTORY=%cd%
 rem 
-rem AFS2ADX2AAC-HC V2 User Interaction
+rem AFS2ADX 2 AAC-HC
 rem 
 for %%f in (ROM\*.afs) do (copy /Y %%f %WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\)
 cd %WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\
 AFSExplorer.exe SH_VOICE_E.afs
+for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\*.afs) do (del /F %%f)
+for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\FULL_AFS_FILE_DUMP\*.adx) do (move /Y %%f %WORKING_DIRECTORY%\Tools\AudioConversion\0ADX2WAV\)
+cd %WORKING_DIRECTORY%\Tools\AudioConversion\0ADX2WAV
+call ADX2WAV2.bat
+mkdir "%WORKING_DIRECTORY%\ROM\Character Speech"
+for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2\*.m4a) do (move /Y %%f "%WORKING_DIRECTORY%\ROM\Character Speech")
+for %%f in (%WORKING_DIRECTORY%\ROM\*.afs) do (del /F %%f)
 rem 
 rem SOFDEC VIDEO TO X265
 rem 
@@ -28,17 +35,12 @@ for %%f in (C:\Temp\x265\*.avi) do (move /Y %%f ROM\Movies)
 rem Cleanup after conversion.
 RD /S /Q "C:\Temp"
 rem 
-rem ADX AUDIO TO AAC-HC
+rem ADX AUDIO TO AAC-HC V2
 rem 
-for %%f in (ROM\*.afs) do (copy /Y %%f %WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\)
-cd %WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\
-AFSExplorer.exe SH_VOICE_E.afs
-for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\*.afs) do (del /F %%f)
-for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\99AFS2ADX\FULL_AFS_FILE_DUMP\*.adx) do (move /Y %%f %WORKING_DIRECTORY%\Tools\AudioConversion\98AFSADX2WAV)
-cd %WORKING_DIRECTORY%\Tools\AudioConversion\98AFSADX2WAV
-call vgmstream.bat
-rem m4a happened inbetween.
-mkdir "%WORKING_DIRECTORY%\ROM\Character Speech"
-for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2\*.m4a) do (move /Y %%f "%WORKING_DIRECTORY%\ROM\Character Speech")
-for %%f in (%WORKING_DIRECTORY%\ROM\*.afs) do (del /F %%f)
+for %%f in (ROM\*.adx) do (copy /Y %%f Tools\AudioConversion\0ADX2WAV\)
+cd Tools\AudioConversion\0ADX2WAV\
+call ADX2WAV.bat
+mkdir %WORKING_DIRECTORY%\ROM\BGM
+for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2\*.m4a) do (move /Y %%f %WORKING_DIRECTORY%\ROM\BGM)
+for %%f in (%WORKING_DIRECTORY%\ROM\*.adx) do (del /f %%f)
 pause
--- Tools/AudioConversion/0ADX2WAV/ADX2WAV2.bat
+++ Tools/AudioConversion/0ADX2WAV/ADX2WAV2.bat
@@ -1,5 +1,5 @@
 rem 1 liner by Sewer56lol
-for %%f in (*.adx) do (vgmstream.exe -o %%~nf.wav %%f)
+for %%f in (*.adx) do (adx2wav.exe %cd%\%%f %cd%\%%~nf.wav)
 for %%f in (*.wav) do (move /Y %%f %WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2)
 for %%f in (*.adx) do (del /f %%f)
 cd %WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2
