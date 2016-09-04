de2c6b1537631d4c50fd2d4f9e939288d74e20b7
Sewer <sewer56lol@googlemail.com>
2016-09-04 17:03:31 +0100

    Windows/CMD: Patch for Dynamic ROM Directory

--- SetupFromROM.bat
+++ SetupFromROM.bat
@@ -21,16 +21,16 @@ rem
 rem SOFDEC VIDEO TO X265
 rem 
 rem Copy Sofdec videos for processing.
-for %%f in (ROM\*.sfd) do (copy /Y %%f Tools\VideoConversion\0SFD2MPG)
-cd Tools\VideoConversion\0SFD2MPG\
+for %%f in (%WORKING_DIRECTORY%\ROM\*.sfd) do (copy /Y %%f %WORKING_DIRECTORY%\Tools\VideoConversion\0SFD2MPG)
+cd %WORKING_DIRECTORY%\Tools\VideoConversion\0SFD2MPG\
 rem Call ConvertHeroes to demux and convert SFD.
 call ConvertHeroes.bat
 rem Convert With Handbrake
 call %WORKING_DIRECTORY%\Tools\VideoConversion\1Handbrake\HeroesToX265.bat
 cd %WORKING_DIRECTORY%
 rem Remove all old cutscene files.
-for %%f in (ROM\*.sfd) do (del /f %%f)
-mkdir ROM\Movies
+for %%f in (%WORKING_DIRECTORY%\ROM\*.sfd) do (del /f %%f)
+mkdir %WORKING_DIRECTORY%\ROM\Movies
 rem Copy all new cutscene x265 encodings.
 for %%f in (C:\Temp\x265\*.avi) do (move /Y %%f ROM\Movies)
 rem Cleanup after conversion.
@@ -38,8 +38,8 @@ RD /S /Q "C:\Temp"
 rem 
 rem ADX AUDIO TO AAC-HC V2
 rem 
-for %%f in (ROM\*.adx) do (copy /Y %%f Tools\AudioConversion\0ADX2WAV\)
-cd Tools\AudioConversion\0ADX2WAV\
+for %%f in (%WORKING_DIRECTORY%\ROM\*.adx) do (copy /Y %%f Tools\AudioConversion\0ADX2WAV\)
+cd %WORKING_DIRECTORY%\Tools\AudioConversion\0ADX2WAV\
 call ADX2WAV.bat
 mkdir "%WORKING_DIRECTORY%\ROM\Background Music"
 for %%f in (%WORKING_DIRECTORY%\Tools\AudioConversion\1WAV2AACHCv2\*.m4a) do (move /Y %%f "%WORKING_DIRECTORY%\ROM\Background Music")
