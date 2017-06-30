5c188b31a9ab25ea76348c99e182f1d9f625f54b
sewer56lol <sewer56lol@googlemail.com>
2017-06-30 20:55:35 +0100

    Add project files.

---
+++ HeroesCollisionGeneratorII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeroesCollisionGeneratorII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
